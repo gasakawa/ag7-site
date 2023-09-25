02aa5c63ff4f28481de009278c29e24206ae806b
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2023-09-25 11:08:53 -0300

    remove i18n settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['pt-BR'],
-    defaultLocale: 'pt-BR'
-  },
   images: {
     domains: ['ag7digital-site.s3.amazonaws.com']
   },
